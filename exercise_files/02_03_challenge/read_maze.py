9786444543d04db5ee032feb20e0b4581bc56fa1
Stefanie Kobsar Rodríguez <stkobsar7@gmail.com>
2021-02-22 20:30:36 +0100

    Course code material

---
+++ exercise_files/02_03_challenge/read_maze.py
@@ -0,0 +1,32 @@
+"""
+Python Data Structures - A Game-Based Approach
+Reading a maze from a text file
+Robin Andrews - https://compucademy.net/
+"""
+
+
+def read_maze(file_name):
+    """
+    Reads a maze stored in a text file and returns a 2d list containing the maze representation.
+    """
+    try:
+        with open(file_name) as fh:
+            maze = [[char for char in line.strip("\n")] for line in fh]
+            num_cols_top_row = len(maze[0])
+            for row in maze:
+                if len(row) != num_cols_top_row:
+                    print("The maze is not rectangular.")
+                    raise SystemExit
+            return maze
+    except OSError:
+        print("There is a problem with the file you have selected.")
+        raise SystemExit
+
+
+if __name__ == "__main__":
+
+    path_to_file = "/Users/Stephi/Documents/programming/REPOSITORIES/DataStructures/exercise_files/02_03_challenge/mazes/challenge_maze.txt"
+    maze = read_maze(path_to_file)
+
+    for row in maze:
+        print(row)
